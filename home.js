1615eb6c6d2e750ef1c77049f9dc3e6907aaca7d
excf28 <deee4444@gmail.com>
2023-09-28 22:48:04 +0200

    animation

--- home.js
+++ home.js
@@ -14,4 +14,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    const strangerContainter = document.getElementById('strangerContainter');
+    const strangerClosed = document.getElementById('strangerClosed');
+    const strangerOpen = document.getElementById('strangerOpen');
+
+    if(strangerContainter && strangerClosed && strangerOpen){
+        let open = false;
+        strangerContainter.addEventListener('click', () => {
+            open = !open;
+            if(open){
+                strangerClosed.classList.add('opacity-0', 'z-n1');
+                strangerOpen.classList.remove('opacity-0', 'z-n1');
+            }else{
+                strangerClosed.classList.remove('opacity-0', 'z-n1');
+                strangerOpen.classList.add('opacity-0', 'z-n1');
+            }
+        })
+    }
+
 });
